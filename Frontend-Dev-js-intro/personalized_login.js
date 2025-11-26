7f1dc6ab064db7540bce358fa0d7dc06f3f0f0c7
parthteotia <parthteotia9@gmail.com>
2025-11-26 15:53:35 +0530

    done

---
+++ Frontend-Dev-js-intro/personalized_login.js
@@ -0,0 +1,16 @@
+let userName= "Raj";
+let currentHour= new Date().getHours();
+
+
+
+if (currentHour<12) {
+    console.log(`Good Morning ${userName}`)
+    
+} 
+else if(currentHour>12 && currentHour<17){
+    console.log(`Good Afternoon ${userName}`)
+}
+else{
+    console.log(`Good Evening ${userName}`)
+    
+}
